f9473e70293028c43ad986e5140ace4c33d79e11
Arian Gallardo <gallardo.a@pucp.pe>
2019-10-02 00:00:51 -0500

    Wizard 2 casi terminado

--- frontend/src/components/Wizard1.vue
+++ frontend/src/components/Wizard1.vue
@@ -6,18 +6,45 @@
                 Primer paso
             </tab-content>
             <tab-content title="Elige tu cuenta">
-                Segundo paso
+                <div id="form2">
+                    <h2 id="IngDatos">INGRESA TUS DATOS</h2>
+                    <h6>Nos interesa conocerte para poder brindarte nuestros servicios a tu medida</h6>
+                    <br><br>
+                    <form>
+                        <div class="form-group row">
+                            <label for="TelfCel" class="col-sm-2 col-form-label">Telefono Celular:</label>
+                            <div class="col-sm-10 inputs">
+                                <input type="text" class="form-control inputs" id="TelfCel" value="">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="TelfCelOp" class="col-sm-2 col-form-label">Teléfono Celular Alternativo:</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control inputs" id="TelfCelOp" value="">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="Correo" class="col-sm-2 col-form-label">Correo Electrónico:</label>
+                            <div class="col-sm-10 inputs">
+                                <input type="text" class="form-control inputs" id="Correo" value="email@example.com">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="CorreoAlt" class="col-sm-2 col-form-label">Correo Electrónico Alternativo:</label>
+                            <div class="col-sm-10 inputs">
+                                <input type="text" class="form-control inputs" id="CorreoAlt" value="email@example.com">
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </tab-content>
             <tab-content title="Términos y condiciones">
                 Tercer paso
-            </tab-content>
-            <tab-content title="Resumen">
-                Cuarto paso
             </tab-content>
         </form-wizard>
     </div>
 </template>
 
-<style src="@/styles/Wizard.css" scoped>
+<style src="@/styles/Wizard.css" scoped>S
 
 </style>
