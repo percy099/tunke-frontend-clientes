1be04206a46591a5ab3fea57e42b8c33ad01ba0a
Percy Quezada <percy.098@hotmail.com>
2019-10-02 04:47:35 -0500

    Conversión a componente el paso2 de no cliente

--- frontend/src/components/Wizard1.vue
+++ frontend/src/components/Wizard1.vue
@@ -3,42 +3,12 @@
         
         <form-wizard next-button-text="Siguiente" title="" subtitle="" color="#2CFFBA" shape="circle" 
         back-button-text="Atrás" finish-button-text="Abrir cuenta" @on-complete="onComplete"
-        step-size="sm" id="container>
+        step-size="sm" id="container">
             <tab-content title="Identifícate">
                 Primer paso
             </tab-content>
             <tab-content title="Permítenos conocerte">
-                <div id="form2">
-                    <h2 id="IngDatos">INGRESA TUS DATOS</h2>
-                    <h6>Nos interesa conocerte para poder brindarte nuestros servicios a tu medida</h6>
-                    <br><br>
-                    <form>
-                        <div class="form-group row">
-                            <label for="TelfCel" class="col-sm-2 col-form-label">Telefono Celular:</label>
-                            <div class="col-sm-10 inputs">
-                                <input type="text" class="form-control inputs" id="TelfCel" value="">
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label for="TelfCelOp" class="col-sm-2 col-form-label">Teléfono Celular Alternativo:</label>
-                            <div class="col-sm-10">
-                                <input type="text" class="form-control inputs" id="TelfCelOp" value="">
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label for="Correo" class="col-sm-2 col-form-label">Correo Electrónico:</label>
-                            <div class="col-sm-10 inputs">
-                                <input type="text" class="form-control inputs" id="Correo" value="email@example.com">
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label for="CorreoAlt" class="col-sm-2 col-form-label">Correo Electrónico Alternativo:</label>
-                            <div class="col-sm-10 inputs">
-                                <input type="text" class="form-control inputs" id="CorreoAlt" value="email@example.com">
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                <Step2NoClient></Step2NoClient>
             </tab-content>
             <tab-content title="Elige tu cuenta" class="">
                 <Step3NoClient></Step3NoClient>
@@ -47,12 +17,13 @@
     </div>
 </template>
 
-<style src="@/styles/Wizard.css" scoped>S
+<style src="@/styles/Wizard.css" scoped>
 
 </style>
 
 <script>
 
+import Step2NoClient from '@/components/Step2NoClient.vue'
 import Step3NoClient from '@/components/Step3NoClient.vue'
 
 export default {
@@ -62,6 +33,7 @@ export default {
         }
     },
     components:{
+        Step2NoClient,
         Step3NoClient
     }
 }
