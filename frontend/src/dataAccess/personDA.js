950ba30bf782d5387c6bf6516fd3828eeb766354
percy099 <44418654+percy099@users.noreply.github.com>
2019-10-04 15:08:58 -0500

    Update personDA.js

--- frontend/src/dataAccess/personDA.js
+++ frontend/src/dataAccess/personDA.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 export function doDniValidation(dni){
-    let url = process.env.VUE_APP_API_URL + 'api/dniValidation
+    let url = process.env.VUE_APP_API_URL + 'api/dniValidation'
     var body ={
         "documentNumber" : dni
     }
     
-    return axios.post(VUE_APP_API_URL,body);
+    return axios.post(url,body);
 }
